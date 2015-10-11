be4709b3e56de24ff2599ef16c9bb8af8e2087cd
Sujoy Chatterjee <sujoychatts@gmail.com>
2015-10-11 09:05:17 +0530

    removed need to re render masonry on window.onload by populating image tags with appropriate height and width before initialing masonry, stop layout on masonry init, on layout complete show transform transition

--- assets/react/components/home.js
+++ assets/react/components/home.js
@@ -16,21 +16,34 @@ module.exports = React.createClass({
 	},
 	initiateMasonry: function(){
 		var cardContainer = document.getElementsByClassName('cards-container')[0];
-		this.masonry = new Masonry(cardContainer, {
-			itemSelector: '.card',
-			columnWidth: '.grid-sizer',
-			gutter: 10
-		})
+		if(!this.masonry){
+			this.masonry = new Masonry(cardContainer, {
+				itemSelector: '.card',
+				columnWidth: '.grid-sizer',
+				gutter: 10,
+				isInitLayout: false
+			});
+			this.masonry.on('layoutComplete', function(){
+				document.getElementsByClassName('cards-container')[0].className = "cards-container show-cards";
+			});
+			this.masonry.layout();
+		}
 	},
 	componentDidMount: function(){
 		this.initiateMasonry();
-		window.onload = this.initiateMasonry;
 	},
 	getImageTag: function(content_text){
+		var img_tag = content_text.match('img[^>]*src[^>]*');
+		if(img_tag){
+			var width = parseInt(img_tag[0].match(/width="\d*/)[0].slice(7)),
+			height = parseInt(img_tag[0].match(/height="\d*/)[0].slice(8)),
+			cardImageWidth = (230 - 6),
+			cardImageHeight = (cardImageWidth/width)*height;
+		}
 		var image_url = content_text ? content_text.match('src="[^"]*"') : undefined;
 		if(image_url){
 			var url = image_url[0].slice(5, (image_url.length - 2));
-			return <img src={url} className="post-image"></img>
+			return <img src={url} height={cardImageHeight + 'px'} width={cardImageWidth + 'px'} className="post-image"></img>
 		}
 	},
 	render: function () {
