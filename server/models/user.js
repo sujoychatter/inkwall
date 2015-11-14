f62c83274425e05e26084bc2f9487b8edf0814a9
Sujoy Chatterjee <sujoychatts@gmail.com>
2015-11-14 18:23:24 +0530

    added photo_large key to users model

--- server/models/user.js
+++ server/models/user.js
@@ -2,15 +2,15 @@ var connection_config = require(_dir.CONFIG + '/knexfile');
 var knex = require('knex')(connection_config[Configs.NODE_ENV]);
 var modelHelper = require(_dir.DIR_HELPERS + '/model_helper');
 
-var show_keys = ['name', 'photo', 'admin', 'id', 'profile_id']
-var user_show_keys = ['name', 'photo', 'admin', 'id', 'email', "profile_id"]
-var admin_show_keys = ['name', 'photo', 'admin', 'id', 'email', "profile_id"]
+var show_keys = ['name', 'photo', 'admin', 'id', 'profile_id', "photo_large"]
+var user_show_keys = ['name', 'photo', 'admin', 'id', 'email', "profile_id", "photo_large"]
+var admin_show_keys = ['name', 'photo', 'admin', 'id', 'email', "profile_id", "photo_large"]
 
 var user_editable_keys = ['email', 'name']
 var admin_editable_keys = ['email', 'name', 'admin', 'photo']
 module.exports = {
 	find: function(id, user){
-		var keys = user && user.admin ? admin_show_keys : (user && user.id == id ? user_show_keys : [] )
+		var keys = user && user.admin ? admin_show_keys : (user && user.id == id ? user_show_keys : show_keys )
 		return knex('users').select(keys).where('id', id);
 	},
 	update: function(id, params, user){
