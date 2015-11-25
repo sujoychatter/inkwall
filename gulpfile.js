11df7556cc8a40e17f83d8d38c4f3421a6cdb29d
Ritesh <ritesh.kumar@housing.com>
2015-11-25 19:48:00 +0530

    logo and minification done

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var babel = require('gulp-babel');
 var sass = require('gulp-sass');
 var babelify = require('babelify');
 var browserify = require('browserify');
-//var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var nodeInspector = require('gulp-node-inspector');
 var nodemon = require('gulp-nodemon');
 var shell = require('gulp-shell');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
 
 // gulp.task('concatapp', function() {
 //   return gulp.src(['assets/vendor/essentials/**/*.js'])
@@ -32,6 +33,8 @@ gulp.task('browserify:js', function () {
     	)
 		.bundle()
 		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(uglify())
 		.pipe(gulp.dest('public/react/'));
 });
 
